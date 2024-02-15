06e121ac5b8e78c117564d4e76d36d76855c9606
MarcieD1 <marciatarisai@icloud.com>
2024-02-14 23:04:11 -0600

    Remove

--- backend/authentication.py
+++
@@ -1,7 +0,0 @@
-# backend/authentication.py
-import hashlib
-
-def hash_password(password):
-    # Implement your password hashing logic here
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    return hashed_password
--- ticket_system.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/bash
-
-python cli/ticket_cli.py "$@"
